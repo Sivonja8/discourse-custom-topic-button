c009b41cd0fda1391da9bdfeec533e2adddaf1ba
Logaut <47109267+Sivonja8@users.noreply.github.com>
2022-05-07 21:57:21 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,4 +6,7 @@
 register_asset 'lib/commands.js'
 register_asset 'lib/index.js'
 register_asset 'lib/jsonrpc.js'
+register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/commands.js'
+register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/index.js'
+register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/jsonrpc.js'
 enabled_site_setting :custom_topic_button_enabled
