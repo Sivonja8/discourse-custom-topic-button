f8bc9d02d6113a234ec44d65ae007e75038d25ea
Logaut <47109267+Sivonja8@users.noreply.github.com>
2022-05-08 11:39:46 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,10 +3,4 @@
 # version: 0.2
 # authors: Robin Ward
 # url: https://github.com/techapj/discourse-custom-topic-button
-register_asset 'lib/commands.js', :server_side
-register_asset 'lib/index.js', :server_side
-register_asset 'lib/jsonrpc.js', :server_side
-register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/commands.js', :server_side
-register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/index.js', :server_side
-register_asset 'javascripts/discourse/connectors/after-topic-footer-main-buttons/jsonrpc.js', :server_side
 enabled_site_setting :custom_topic_button_enabled
