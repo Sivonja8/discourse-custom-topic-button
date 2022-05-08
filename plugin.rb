151d4c5564392a290cde62019c424929d73acd5a
Logaut <47109267+Sivonja8@users.noreply.github.com>
2022-05-08 15:51:49 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,5 +3,5 @@
 # version: 0.2
 # authors: Robin Ward
 # url: https://github.com/techapj/discourse-custom-topic-button
-gem 'bitcoiner', '~> 0.2.1'
+gem 'bitcoiner', '0.2.1'
 enabled_site_setting :custom_topic_button_enabled
